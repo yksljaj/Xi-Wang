d8e266b72dc123f99106ba7bb09d9f35b16fe3c3
Jessie <jessie@2017de-Mac-mini.local>
2018-03-12 17:22:32 +0800

    Initial Commit

---
+++ Xi Wang/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Xi Wang
+//
+//  Created by Jessie on 2018/3/12.
+//  Copyright © 2018年 Jessie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Xi Wang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xi Wang
+//
+//  Created by Jessie on 2018/3/12.
+//  Copyright © 2018年 Jessie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
